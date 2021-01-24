c47a48ebb3c3e7fcf3c4143674a6a602d7324245
Jason Deng <foralief@gmail.com>
2021-01-24 16:43:06 -0600

    Finished color-palette

--- App.js
+++ App.js
@@ -1,17 +1,65 @@
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		let initialState = {
+			colors: [
+				{hex: "#54c06c", isLocked: false},
+				{hex: "#e4d623", isLocked: false},
+				{hex: "#b5453f", isLocked: false},
+				{hex: "#e8630c", isLocked: false},
+				{hex: "#5d1170", isLocked: false},
+			],
+		};
+
+		this.state = initialState;
+	}
+
+	toggleLock(i) {
+		let newColors = this.state.colors.map((color, index) => {
+			if (i === index) {
+				return {
+					...color,
+					isLocked: !color.isLocked,
+				};
+			} else {
+				return color;
+			}
+		});
+
+		this.setState({
+			colors: newColors,
+		});
+	}
+
+	randomize() {
+		let newColors = this.state.colors.map((color, idnex) => {
+			if (!color.isLocked) {
+				return {
+					...color,
+					hex: "#" + Math.floor(Math.random() * 16777215).toString(16),
+				};
+			} else {
+				return color;
+			}
+		});
+
+		this.setState({
+			colors: newColors,
+		});
 	}
 
 	render() {
 		return (
 			<div>
-				<div class="text-center bg-dark fixed-top">
-					<button class="btn btn-secondary m-1">RANDOMIZE COLORS</button>
+				<div className="text-center bg-dark fixed-top">
+					<button className="btn btn-secondary m-1" onClick={() => this.randomize()}>
+						RANDOMIZE COLORS
+					</button>
 				</div>
-				<div>
-					<Color />
+				<div className="w-100 d-flex" style={{height: "100vh"}}>
+					{this.state.colors.map((color, index) => (
+						<Color color={color} key={index} toggle={() => this.toggleLock(index)} />
+					))}
 				</div>
 			</div>
 		);
--- Color.js
+++ Color.js
@@ -1,7 +1,16 @@
 const Color = props => {
+	let hex = props.color.hex;
+	let isLocked = props.color.isLocked;
+
 	return (
-		<div style={{color: red}}>
-			<h1>Color</h1>
+		<div
+			style={{backgroundColor: hex}}
+			className="w-100 d-flex flex-column align-items-center justify-content-center"
+		>
+			<h1>{hex}</h1>
+			<button className={isLocked ? "btn btn-dark" : "btn btn-outline-dark"} onClick={props.toggle}>
+				{isLocked ? "UNLOCK" : "LOCK"}
+			</button>
 		</div>
 	);
 };
